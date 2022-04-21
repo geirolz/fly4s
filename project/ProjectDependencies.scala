da2da241bc871dbe09475ec51b193f3a46bd9f9d
Scala Steward <me@scala-steward.org>
2022-04-21 14:32:52 +0200

    Update flyway-core to 8.5.9

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -33,7 +33,7 @@ object ProjectDependencies {
 
   private val db: Seq[ModuleID] = {
     Seq(
-      "org.flywaydb" % "flyway-core" % "8.5.8"
+      "org.flywaydb" % "flyway-core" % "8.5.9"
     )
   }
 }
