0307e95e0ea200a5ad309f27374b365fe3c2ce59
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2023-11-26 00:12:59 +0000

    Update flyway-core to 10.1.0

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -5,7 +5,7 @@ object ProjectDependencies {
   lazy val common: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-core" % "2.10.0",
     "org.typelevel" %% "cats-effect" % "3.5.2",
-    "org.flywaydb" % "flyway-core" % "9.22.3",
+    "org.flywaydb" % "flyway-core" % "10.1.0",
     // test
     "org.scalameta" %% "munit" % "0.7.29" % Test,
     "org.typelevel" %% "munit-cats-effect-3" % "1.0.7" % Test
