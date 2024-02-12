20c6e9ba437832b4c66a114cc58565eb77d6a3e7
geirolz <david.geirola@gmail.com>
2024-02-12 23:13:44 +0100

    Refactor

--- core/src/main/scala/fly4s/data/LoggerType.scala
+++ core/src/main/scala/fly4s/data/LoggerType.scala
@@ -1,30 +1,22 @@
 package fly4s.data
 
-sealed trait LoggerType
+abstract sealed class LoggerType(val tpeValue: String)
 object LoggerType {
-  case object Auto extends LoggerType
-  case object Console extends LoggerType
-  case object Slf4j extends LoggerType
-  case object Log4j2 extends LoggerType
-  case object ApacheCommons extends LoggerType
-  case class Custom(fullyQualifiedClassName: String) extends LoggerType
+  case object Auto extends LoggerType("auto")
+  case object Console extends LoggerType("console")
+  case object Slf4j extends LoggerType("slf4j")
+  case object Log4j2 extends LoggerType("log4j2")
+  case object ApacheCommons extends LoggerType("apache-commons")
+  case class Custom(fullyQualifiedClassName: String) extends LoggerType(fullyQualifiedClassName)
+
+  def toFlywayValue(ltype: LoggerType): String = ltype.tpeValue
 
   def fromFlywayValue(value: String): LoggerType = value match {
-    case "auto"           => Auto
-    case "console"        => Console
-    case "slf4j"          => Slf4j
-    case "log4j2"         => Log4j2
-    case "apache-commons" => ApacheCommons
-    case custom           => Custom(custom)
+    case Auto.tpeValue          => Auto
+    case Console.tpeValue       => Console
+    case Slf4j.tpeValue         => Slf4j
+    case Log4j2.tpeValue        => Log4j2
+    case ApacheCommons.tpeValue => ApacheCommons
+    case custom                 => Custom(custom)
   }
-
-  def toFlywayValue(ltype: LoggerType): String =
-    ltype match {
-      case LoggerType.Auto                            => "auto"
-      case LoggerType.Console                         => "console"
-      case LoggerType.Slf4j                           => "slf4j"
-      case LoggerType.Log4j2                          => "log4j2"
-      case LoggerType.ApacheCommons                   => "apache-commons"
-      case LoggerType.Custom(fullyQualifiedClassName) => fullyQualifiedClassName
-    }
 }
