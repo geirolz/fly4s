5f266fe5b058224f80aa103f17c2620e6499418d
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-02-25 14:46:47 +0000

    Update fluent-copy to 0.0.2

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -3,23 +3,23 @@ import sbt._
 object ProjectDependencies {
 
   lazy val common: Seq[ModuleID] = Seq(
-    "org.typelevel" %% "cats-core" % "2.10.0",
+    "org.typelevel" %% "cats-core"   % "2.10.0",
     "org.typelevel" %% "cats-effect" % "3.5.2",
-    "org.flywaydb" % "flyway-core" % "10.4.1",
+    "org.flywaydb"   % "flyway-core" % "10.4.1",
     // test
-    "org.scalameta" %% "munit" % "0.7.29" % Test,
-    "org.typelevel" %% "munit-cats-effect-3" % "1.0.7" % Test,
-    "com.h2database" % "h2" % "2.2.224" % Test
+    "org.scalameta" %% "munit"               % "0.7.29"  % Test,
+    "org.typelevel" %% "munit-cats-effect-3" % "1.0.7"   % Test,
+    "com.h2database" % "h2"                  % "2.2.224" % Test
   )
 
   lazy val for2_13_Only: Seq[ModuleID] = Seq(
-    "com.github.geirolz" %% "fluent-copy" % "0.0.1"
+    "com.github.geirolz" %% "fluent-copy" % "0.0.2"
   )
 
   object Plugins {
     val compilerPluginsFor2_13: Seq[ModuleID] = Seq(
-      compilerPlugin("org.typelevel" %% "kind-projector" % "0.13.2" cross CrossVersion.full),
-      compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
+      compilerPlugin("org.typelevel" %% "kind-projector"     % "0.13.2" cross CrossVersion.full),
+      compilerPlugin("com.olegpy"    %% "better-monadic-for" % "0.3.1")
     )
 
     val compilerPluginsFor3: Seq[ModuleID] = Nil
