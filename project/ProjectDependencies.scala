f7e73a54dc0dce6502ffdf8a6cf19c3c9cbc8fad
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-08-18 00:25:55 +0000

    Update munit to 1.0.1

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -7,7 +7,7 @@ object ProjectDependencies {
     "org.typelevel" %% "cats-effect" % "3.5.4",
     "org.flywaydb"   % "flyway-core" % "10.17.0",
     // test
-    "org.scalameta" %% "munit"             % "1.0.0"   % Test,
+    "org.scalameta" %% "munit"             % "1.0.1"   % Test,
     "org.typelevel" %% "munit-cats-effect" % "2.0.0"   % Test,
     "com.h2database" % "h2"                % "2.3.230" % Test
   )
