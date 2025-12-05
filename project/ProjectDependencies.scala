f87a2af9dbd6725ff0bead8b31044838c4fa4e1f
geirolz <david.geirola@gmail.com>
2025-12-05 20:56:12 +0100

    Update ProjectDependencies to latest library versions: cats-effect to 3.6.3, flyway-core to 11.18.0, munit to 1.2.1, munit-cats-effect to 2.1.0, and h2 to 2.4.240. Also, update kind-projector compiler plugin to 0.13.4.

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -4,12 +4,12 @@ object ProjectDependencies {
 
   lazy val common: Seq[ModuleID] = Seq(
     "org.typelevel" %% "cats-core"   % "2.13.0",
-    "org.typelevel" %% "cats-effect" % "3.5.7",
-    "org.flywaydb"   % "flyway-core" % "11.1.1",
+    "org.typelevel" %% "cats-effect" % "3.6.3",
+    "org.flywaydb"   % "flyway-core" % "11.18.0",
     // test
-    "org.scalameta" %% "munit"             % "1.0.4"   % Test,
-    "org.typelevel" %% "munit-cats-effect" % "2.0.0"   % Test,
-    "com.h2database" % "h2"                % "2.3.232" % Test
+    "org.scalameta" %% "munit"             % "1.2.1"   % Test,
+    "org.typelevel" %% "munit-cats-effect" % "2.1.0"   % Test,
+    "com.h2database" % "h2"                % "2.4.240" % Test
   )
 
   lazy val for2_13_Only: Seq[ModuleID] = Seq(
@@ -18,7 +18,7 @@ object ProjectDependencies {
 
   object Plugins {
     val compilerPluginsFor2_13: Seq[ModuleID] = Seq(
-      compilerPlugin("org.typelevel" %% "kind-projector"     % "0.13.3" cross CrossVersion.full),
+      compilerPlugin("org.typelevel" %% "kind-projector"     % "0.13.4" cross CrossVersion.full),
       compilerPlugin("com.olegpy"    %% "better-monadic-for" % "0.3.1")
     )
 
